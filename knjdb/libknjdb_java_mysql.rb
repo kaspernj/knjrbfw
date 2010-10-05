bdffe7a4375e6815128edd5536df0d1f5c97d316
Kasper Johansen <k@spernj.org>
2010-10-05 11:25:16 +0200

    Added java-mysql driver for JRuby. Added mysql-connector for the mysql java driver.

---
+++ knjdb/libknjdb_java_mysql.rb
@@ -0,0 +1,98 @@
+require "java"
+
+class KnjDB_java_mysql
+	def escape_table
+		return "`"
+	end
+	
+	def escape_col
+		return "`"
+	end
+	
+	def escape_val
+		return "'"
+	end
+	
+	def initialize(knjdb_ob)
+		@knjdb = knjdb_ob
+		@opts = knjdb_ob.opts
+		
+		if knjdb_ob.opts.has_key?(:port)
+			@port = knjdb_ob.opts[:port].to_i
+		else
+			@port = 3306
+		end
+		
+		require File.dirname(__FILE__) + "/mysql-connector-java-5.1.13/mysql-connector-java-5.1.13-bin.jar"
+		import "com.mysql.jdbc.Driver"
+		self.reconnect
+		self.query("SET SQL_MODE = ''")
+	end
+	
+	def reconnect
+		@conn = java.sql::DriverManager.getConnection("jdbc:mysql://#{@opts[:host]}:#{@port}/#{@opts[:db]}?user=#{@opts[:user]}&password=#{@opts[:pass]}&populateInsertRowWithDefaultValues=true&zeroDateTimeBehavior=round")
+	end
+	
+	def query(sqlstr)
+		stmt = @conn.createStatement
+		
+		if sqlstr.match(/insert\s+into\s+/i) or sqlstr.match(/update\s+/i)
+			rs = stmt.execute(sqlstr)
+		else
+			rs = stmt.executeQuery(sqlstr)
+		end
+		
+		return KnjDB_java_mysql_result.new(rs)
+	end
+	
+	def escape(string)
+		return string.gsub("'", "\\'")
+	end
+	
+	def lastID
+		data = self.query("SELECT LAST_INSERT_ID() AS id").fetch
+		return data[:id]
+	end
+	
+	def destroy
+		@conn = nil
+		@knjdb = nil
+		@opts = nil
+		@port = nil
+	end
+end
+
+class KnjDB_java_mysql_result
+	def initialize(result)
+		@result = result
+	end
+	
+	def read_meta
+		@result.before_first
+		meta = @result.meta_data
+		
+		@keys = []
+		0.upto(meta.column_count - 1) do |count|
+			@keys << meta.column_name(count + 1).to_sym
+		end
+	end
+	
+	def fetch
+		self.read_meta if !@keys
+		status = @result.next
+		return false if !status
+		
+		ret = {}
+		if $db and $db.opts[:return_keys] == "symbols"
+			0.upto(@keys.length - 1) do |count|
+				ret[@keys[count]] = @result.string(count + 1)
+			end
+		else
+			0.upto(@keys.length - 1) do |count|
+				ret[@keys[count].to_s] = @result.string(count + 1)
+			end
+		end
+		
+		return ret
+	end
+end
---
+++ knjdb/mysql-connector-java-5.1.13/src/com/mysql/jdbc/PacketTooBigException.java
@@ -0,0 +1,57 @@
+/*
+ Copyright  2002-2004 MySQL AB, 2008 Sun Microsystems
+ All rights reserved. Use is subject to license terms.
+
+  The MySQL Connector/J is licensed under the terms of the GPL,
+  like most MySQL Connectors. There are special exceptions to the
+  terms and conditions of the GPL as it is applied to this software,
+  see the FLOSS License Exception available on mysql.com.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; version 2 of the
+  License.
+
+  This program is distributed in the hope that it will be useful,  
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA
+
+
+
+ */
+package com.mysql.jdbc;
+
+import java.sql.SQLException;
+
+/**
+ * Thrown when a packet that is too big for the server is created.
+ * 
+ * @author Mark Matthews
+ */
+public class PacketTooBigException extends SQLException {
+	// ~ Constructors
+	// -----------------------------------------------------------
+
+	/**
+	 * Creates a new PacketTooBigException object.
+	 * 
+	 * @param packetSize
+	 *            the size of the packet that was going to be sent
+	 * @param maximumPacketSize
+	 *            the maximum size the server will accept
+	 */
+	public PacketTooBigException(long packetSize, long maximumPacketSize) {
+		super(
+				Messages.getString("PacketTooBigException.0") + packetSize + Messages.getString("PacketTooBigException.1") //$NON-NLS-1$ //$NON-NLS-2$
+						+ maximumPacketSize
+						+ Messages.getString("PacketTooBigException.2") //$NON-NLS-1$
+						+ Messages.getString("PacketTooBigException.3") //$NON-NLS-1$
+						+ Messages.getString("PacketTooBigException.4"), SQLError.SQL_STATE_GENERAL_ERROR); //$NON-NLS-1$
+	}
+}
