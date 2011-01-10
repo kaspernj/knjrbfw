a47648c89d745edda5c5bfba33cfe96fac96efde
Kasper Johansen <k@spernj.org>
2011-01-10 21:35:31 +0100

    image is now using Imagemagick instead of GD2 which was broken in Ruby 1.9.

--- exchangerates.rb
+++ exchangerates.rb
@@ -1,10 +1,13 @@
 class Knj::Exchangerates
+	attr_reader :rates
+	
 	def initialize(args = {})
 		@rates = {}
 	end
 	
 	def base=(data)
-		@rates[data[:locale]] = data
+		@base = data[:locale].to_s
+		self.add_rate(data)
 	end
 	
 	def add_rate(data)
@@ -12,13 +15,25 @@ class Knj::Exchangerates
 			raise "Invalid locale given."
 		end
 		
-		@rates[data[:locale]] = data
+		@rates[data[:locale].to_s] = data
 	end
 	
 	def value(locale, floatval)
+		floatval = floatval.to_f
+		locale = locale.to_s
+		
 		raise "No such locale: #{locale}." if !@rates.has_key?(locale)
 		
-		val = @rates[locale][:rate] * floatval
-		return val
+		base_rate = @rates[@base][:rate].to_f
+		cur_rate = @rates[locale][:rate].to_f
+		
+		if base_rate == cur_rate
+			return floatval 
+		elsif cur_rate < base_rate
+			diff = 1 + (base_rate - cur_rate)
+			return floatval * diff
+		else
+			return floatval / cur_rate
+		end
 	end
 end
