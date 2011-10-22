429e64634c5645ba1240b7164ab8b9b34e3e0db2
Kasper Johansen <k@spernj.org>
2011-10-22 17:30:37 +0200

    Latest changes regarding rhodes.

--- lib/knj/knj_controller.rb
+++ lib/knj/knj_controller.rb
@@ -1,7 +1,5 @@
 #This files makes the framework able to receive calls from the Rhodes framework from RhoMobile.
 class KnjController < Rho::RhoController
-	include BrowserHelper
-
 	#GET /Server
 	def index
 		render
