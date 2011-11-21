de819fcc20fd8960ddc134e1fccbd95f11dd18fb
Kasper Johansen <k@spernj.org>
2011-11-21 18:30:33 +0100

    Added support for "ls -lh" which translates 100G as gigabytes (also megabytes and bytes but always returns as bytes).

--- lib/knj/cmd_parser.rb
+++ lib/knj/cmd_parser.rb
@@ -1,16 +1,31 @@
 class Knj::Cmd_parser
-  def self.lsl(str)
+  def self.lsl(str, args = {})
     ret = []
     
     str.lines.each do |line|
-      next if line.match(/^total (\d+)$/)
-      match = line.match(/^(.)(.)(.)(.)(.)(.)(.)(.)(.)(.)\s+(\d+)\s+(.+)\s+([^\W].+?)\s+(\d+)\s+((\d+)-(\d+)-(\d+)|(([A-z]{3})\s+(\d+)))\s+(\d+):(\d+)\s+(.+)$/)
+      next if line.match(/^total ([\d\.]+)(M|k|G|)$/)
+      match = line.match(/^(.)(.)(.)(.)(.)(.)(.)(.)(.)(.)\s+(\d+)\s+(.+)\s+([^\W].+?)\s+([\d\.]+)(M|k|G|)\s+((\d+)-(\d+)-(\d+)|(([A-z]{3})\s+(\d+)))\s+(\d+):(\d+)\s+(.+)$/)
       raise "Could not match: '#{line}'." if !match
       
-      if match[16] and match[17] and match[18]
-        time = Time.local(match[16].to_i, match[17].to_i, match[18].to_i, match[22].to_i, match[23].to_i)
-      elsif match[20] and match[21]
-        time = Time.local(Time.now.year, match[20], match[21], match[22], match[23])
+      if match[17] and match[18] and match[19]
+        time = Time.local(match[17].to_i, match[18].to_i, match[19].to_i, match[23].to_i, match[24].to_i)
+      elsif match[20] and match[21] and match[22]
+        time = Time.local(Time.now.year, match[21], match[22].to_i, match[23].to_i, match[24].to_i)
+      end
+      
+      bytes = match[14].to_f
+      
+      size_match = match[15]
+      if size_match == ""
+        #bytes - dont touch
+      elsif size_match == "k"
+        bytes = bytes * 1024
+      elsif size_match == "M"
+        bytes = bytes * 1024 * 1024
+      elsif size_match == "G"
+        bytes = bytes * 1024 * 1024 * 1024
+      else
+        raise "Unknown size match: '#{size_match}'."
       end
       
       ret << {
@@ -33,9 +48,9 @@ class Knj::Cmd_parser
         },
         :usr => match[12],
         :grp => match[13],
-        :size => match[14].to_i,
+        :size => bytes.to_i,
         :time => time,
-        :file => match[24]
+        :file => match[25]
       }
     end
     
