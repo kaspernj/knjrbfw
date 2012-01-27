8f1c04818893579b4c6d252e2489fa37d60bb859
Kasper Johansen <kaspernj@development.balance4u.com>
2012-01-27 10:59:24 +0100

    Now supports output in Excel 5 by using Spreadsheet framework.

--- lib/knj/table_writer.rb
+++ lib/knj/table_writer.rb
@@ -8,8 +8,12 @@ class Knj::Table_writer
     
     if @args["format"] == "csv"
       @fp = File.open(@args["filepath"], "w")
-    elsif @args["format"] == "excel"
-      raise "Excel not supported."
+    elsif @args["format"] == "excel5"
+      require "spreadsheet"
+      
+      @wb = Spreadsheet::Workbook.new
+      @ws = @wb.create_worksheet
+      @row = 0
     else
       raise "Unsupported format: '#{@args["format"]}'."
     end
@@ -32,6 +36,21 @@ class Knj::Table_writer
       #line_str = line_str.encode("iso8859-1") if @args["encoding"] == "iso8859-1"
       
       @fp.write(line_str)
+    elsif @args["format"] == "excel5"
+      col_count = 0
+      arr.each do |val|
+        if val.is_a?(Hash) and val["type"] == "decimal"
+          @ws[@row, col_count] = Knj::Php.number_format(val["value"], @args["amount_decimals"], @args["amount_dsep"], @args["amount_tsep"])
+        elsif val.is_a?(Hash) and val["type"] == "date"
+          @ws[@row, col_count] = Knj::Php.date(@args["date_format"], val["value"])
+        else
+          @ws[@row, col_count] = val
+        end
+        
+        col_count += 1
+      end
+      
+      @row += 1
     else
       raise "Unsupported format: '#{@args["format"]}'."
     end
@@ -40,6 +59,17 @@ class Knj::Table_writer
   def close
     if @args["format"] == "csv"
       @fp.close
+    elsif @args["format"] == "excel5"
+      dirname = File.dirname(@args["filepath"])
+      basename = File.basename(@args["filepath"], File.extname(@args["filepath"]))
+      
+      temp_path = "#{dirname}/#{basename}.xls"
+      
+      @wb.write(temp_path)
+      @wb = nil
+      @ws = nil
+      
+      FileUtils.mv(temp_path, @args["filepath"])
     else
       raise "Unsupported format: '#{@args["format"]}'."
     end
@@ -48,6 +78,8 @@ class Knj::Table_writer
   def ext
     if @args["format"] == "csv"
       return "csv"
+    elsif @args["format"] == "excel5"
+      return "xls"
     else
       raise "Unsupported format: '#{@args["format"]}'."
     end
@@ -56,6 +88,8 @@ class Knj::Table_writer
   def ftype
     if @args["format"] == "csv"
       return "text/csv"
+    elsif @args["format"] == "excel5"
+      return "application/ms-excel"
     else
       raise "Unsupported format: '#{@args["format"]}'."
     end
