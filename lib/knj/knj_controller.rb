cbc3d60272400b7b79979d9034dc7be0ef26eeb8
Kasper Johansen <k@spernj.org>
2012-03-29 13:11:29 +0200

    Removed WeakRef and Delegate from Rhodes-framework. Added built-in YouTube page with various posibilities for opening YouTube-videos on various platforms.

--- lib/knj/knj_controller.rb
+++ lib/knj/knj_controller.rb
@@ -8,9 +8,18 @@ class KnjController < Rho::RhoController
   def html_links
     System.open_url(@params["url"])
   end
-  
+	
   def callback
     block = $rhodes.callbacks(@params["callback_id"])
     block.call(:params => @params)
+  end
+  
+  def youtube_embed
+		@iframe_height = System.get_property("screen_height").to_i - 40
+		render :file => "Knj/rhodes/youtube_embed.erb"
+  end
+  
+  def youtube_open
+		render :file => "Knj/rhodes/youtube_open.erb"
   end
 end
