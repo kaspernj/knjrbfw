0a8ac79e591c62b9cef0a631219f4d7c371cd7a7
Kasper Johansen <k@spernj.org>
2012-05-03 15:30:50 +0200

    Removed pre-module-names.

--- lib/knj/threadsafe.rb
+++ lib/knj/threadsafe.rb
@@ -2,18 +2,24 @@ require "monitor"
 
 #This module contains various tools to handle thread-safety easily and pretty.
 module Knj::Threadsafe
-  #JRuby can actually corrupt an array. Use this method to only get a protected array when running JRuby and not having to write "if RUBY_ENGINE"-stuff.
+  #JRuby can corrupt an array in a threadded env. Use this method to only get a synchronized array when running JRuby and not having to write "if RUBY_ENGINE"-stuff.
   def self.std_array
-    return Knj::Threadsafe::Synced_array.new if RUBY_ENGINE == "jruby"
+    return Synced_array.new if RUBY_ENGINE == "jruby"
     return []
   end
   
   #Instances of this class proxies calls to a given-object by using a mutex or monitor.
-  class Knj::Threadsafe::Proxy
+  #
+  #==== Examples
+  # threadsafe_array = Knj::Threadsafe::Proxy.new(:obj => [])
+  # threadsafe_array << 5
+  # ret = threadsafe_array[0]
+  #
+  # threadsafe_array = Knj::Threadsafe::Proxy.new(:obj => [], :monitor => true)
+  class Proxy
     #Spawn needed vars.
     def initialize(args)
       if args[:monitor]
-        require "monitor"
         @mutex = Monitor.new
       elsif args[:mutex]
         @mutex = args[:mutex]
@@ -32,8 +38,8 @@ module Knj::Threadsafe
     end
   end
   
-  #This module can be included on a class to make all method-calls synchronized. Examples with array and hash are below.
-  module Knj::Threadsafe::Synced_module
+  #This module can be included on a class to make all method-calls synchronized (by using monitor). Examples with array and hash are below.
+  module Monitored_module
     def self.included(base)
       Knj::Strings.const_get_full(base.to_s).class_eval do
         self.instance_methods.each do |method_name|
@@ -57,13 +63,38 @@ module Knj::Threadsafe
     end
   end
   
+  #This module can be included on a class to make all method-calls synchronized (by using mutex). Examples with array and hash are below.
+  module Mutexed_module
+    def self.included(base)
+      Knj::Strings.const_get_full(base.to_s).class_eval do
+        self.instance_methods.each do |method_name|
+          #These two methods create warnings under JRuby.
+          if RUBY_ENGINE == "jruby"
+            next if method_name == :instance_exec or method_name == :instance_eval
+          end
+          
+          new_method_name = "_ts_#{method_name}"
+          alias_method(new_method_name, method_name)
+          
+          define_method method_name do |*args, &block|
+            #Need to use monitor, since the internal calls might have to run not-synchronized, and we have just overwritten the internal methods.
+            @_ts_mutex = Mutex.new if !@_ts_mutex
+            @_ts_mutex.synchronize do
+              return self._ts___send__(new_method_name, *args, &block)
+            end
+          end
+        end
+      end
+    end
+  end
+  
   #Predefined synchronized array.
-  class Knj::Threadsafe::Synced_array < ::Array
-    include Knj::Threadsafe::Synced_module
+  class Synced_array < ::Array
+    include Mutexed_module
   end
   
   #Predefined synchronized hash.
-  class Knj::Threadsafe::Synced_hash < ::Hash
-    include Knj::Threadsafe::Synced_module
+  class Synced_hash < ::Hash
+    include Mutexed_module
   end
 end
