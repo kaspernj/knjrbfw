7efda1a169d364b74e72c0bca10aad92d71e49e4
Kasper Johansen <k@spernj.org>
2012-05-04 09:29:25 +0200

    Added even more documentation.

--- lib/knj/locale_strings.rb
+++ lib/knj/locale_strings.rb
@@ -1,17 +1,27 @@
 #This file can be symlinked and thereby easily translated by POEdit. Further more the static methods can be used for stuff.
 module Knj::Locales
+  #Returns an array of translated day-names ('_'-method must exist!).
+  #===Examples
+  # Knj::Locales.days_arr #=> ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   def self.days_arr
     return [_("Monday"), _("Tuesday"), _("Wednesday"), _("Thursday"), _("Friday"), _("Saturday"), _("Sunday")]
   end
   
+  #Returns an array of short translated day-names.
+  #===Examples
+  # Knj::Locales.days_short_arr #=> ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
   def self.days_short_arr
     return [_("Mon"), _("Tue"), _("Wed"), _("Thu"), _("Fri"), _("Sat"), _("Sun")]
   end
   
+  #Returns an array of translated month-names.
+  #===Examples
+  # Knj::Locales.months_arr #=> ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   def self.months_arr
     return [_("January"), _("February"), _("March"), _("April"), _("May"), _("June"), _("July"), _("August"), _("September"), _("October"), _("November"), _("December")]
   end
   
+  #Returns an array of translate 'ago'-strings. View the source to see which.
   def self.ago_strings
     return {
       :year_ago_str => _("%s year ago"),
--- lib/knj/locales.rb
+++ lib/knj/locales.rb
@@ -1,5 +1,8 @@
+#This module can be used to handel various language-stuff.
 module Knj::Locales
   #Returns the primary locale, secondary locale and the two put together.
+  #===Examples
+  # Knj::Locales.lang #=> {"first" => "en", "second" => "GB", "full" => "en_GB"}
   def self.lang
     match = self.locale.to_s.match(/^([a-z]{2})_([A-Z]{2})/)
     raise "Could not understand language: #{self.locale}." if !match
@@ -12,6 +15,8 @@ module Knj::Locales
   end
   
   #Returns various localized information about the environment.
+  #===Examples
+  # Knj::Locales.localeconv #=> {"decimal_point" => ".", "thousands_sep" => ",", "csv_delimiter" => ","}
   def self.localeconv
     f = Knj::Locales.lang["first"]
     
@@ -38,18 +43,25 @@ module Knj::Locales
   end
   
   #Returns a float from the formatted string according to the current locale.
+  #===Examples
+  # Knj::Locales.number_in("123,456.68") #=> 123456.68
   def self.number_in(num_str)
     lc = Knj::Locales.localeconv
     return num_str.to_s.gsub(lc["thousands_sep"], "").gsub(lc["decimal_point"], ".").to_f
   end
   
   #Returns the given number as a formatted string according to the current locale.
+  #===Examples
+  # Knj::Locales.number_out(123456.68) #=> "123,456.68"
   def self.number_out(num_str, dec = 2)
     lc = Knj::Locales.localeconv
     return Knj::Php.number_format(num_str, dec, lc["decimal_point"], lc["thousands_sep"])
   end
   
-  #Returns the current locale for the current environment.
+  #Returns the current locale for the current environment (_session[:locale] or Thread.current[:locale]).
+  #===Examples
+  # Knj::Locales.locale #=> 'en_GB'
+  # Knj::Locales.locale #=> 'da_DK'
   def self.locale
     begin
       return _session[:locale] if _session[:locale].to_s.strip.length > 0
