5b75fbee0218131d0dd674c1a4a46643a3e5cb92
Kasper Johansen <k@spernj.org>
2012-05-23 21:46:19 +0200

    Added a lot of documentation. Cleaned up the code.

--- lib/knj/knjdb/drivers/sqlite3/knjdb_sqlite3_columns.rb
+++ lib/knj/knjdb/drivers/sqlite3/knjdb_sqlite3_columns.rb
@@ -1,12 +1,13 @@
+#This class handels the SQLite3-specific behaviour for columns.
 class KnjDB_sqlite3::Columns
-  attr_reader :db, :driver
+  attr_reader :db
   
+  #Constructor. This should not be called manually.
   def initialize(args)
     @args = args
-    @db = @args[:db]
-    @driver = @args[:driver]
   end
   
+  #Returns SQL for a knjdb-compatible hash.
   def data_sql(data)
     raise "No type given." if !data["type"]
     type = data["type"]
@@ -17,7 +18,7 @@ class KnjDB_sqlite3::Columns
     end
     
     data["maxlength"] = 255 if type == "varchar" and !data.key?("maxlength")
-    type = "integer" if @db.int_types.index(type) and (data["autoincr"] or data["primarykey"])
+    type = "integer" if @args[:db].int_types.index(type) and (data["autoincr"] or data["primarykey"])
     
     sql = "`#{data["name"]}` #{type}"
     sql << "(#{data["maxlength"]})" if data["maxlength"] and !data["autoincr"]
@@ -28,29 +29,34 @@ class KnjDB_sqlite3::Columns
     if data.key?("default_func")
       sql << " DEFAULT #{data["default_func"]}"
     elsif data.key?("default") and data["default"] != false
-      sql << " DEFAULT '#{@db.escape(data["default"])}'"
+      sql << " DEFAULT '#{@args[:db].escape(data["default"])}'"
     end
     
     return sql
   end
 end
 
+#This class handels all the SQLite3-columns.
 class KnjDB_sqlite3::Columns::Column
   attr_reader :args
   
+  #Constructor. This should not be called manually.
   def initialize(args)
     @args = args
     @db = @args[:db]
   end
   
+  #Returns the name of the column.
   def name
     return @args[:data][:name]
   end
   
+  #Returns the columns table-object.
   def table
     return @db.tables[@args[:table_name]]
   end
   
+  #Returns the data of the column as a hash in knjdb-format.
   def data
     return {
       "type" => self.type,
@@ -63,6 +69,7 @@ class KnjDB_sqlite3::Columns::Column
     }
   end
   
+  #Returns the type of the column.
   def type
     if !@type
       if match = @args[:data][:type].match(/^([A-z]+)$/)
@@ -91,17 +98,20 @@ class KnjDB_sqlite3::Columns::Column
     return @type
   end
   
+  #Returns true if the column allows null. Otherwise false.
   def null?
     return false if @args[:data][:notnull].to_i == 1
     return true
   end
   
+  #Returns the maxlength of the column.
   def maxlength
-    self.type
+    self.type if !@maxlength
     return @maxlength if @maxlength
     return false
   end
   
+  #Returns the default value of the column.
   def default
     def_val = @args[:data][:dflt_value]
     if def_val.to_s.slice(0..0) == "'"
@@ -116,24 +126,26 @@ class KnjDB_sqlite3::Columns::Column
     return def_val
   end
   
+  #Returns true if the column is the primary key.
   def primarykey?
-    return true if @args[:data][:name] == "id"
+    return true if self.name.to_s == "id"
     return false if @args[:data][:pk].to_i == 0
     return true
   end
   
+  #Returns true if the column is auto-increasing.
   def autoincr?
-    return true if @args[:data][:name] == "id"
+    return true if self.name.to_s == "id"
     return true if @args[:data][:pk].to_i >= 1
     return false
   end
   
+  #Drops the column from the table.
   def drop
-    self.table.copy(
-      "drops" => self.name
-    )
+    self.table.copy("drops" => self.name)
   end
   
+  #Changes data on the column. Like the name, type, maxlength or whatever.
   def change(data)
     newdata = data.clone
     
