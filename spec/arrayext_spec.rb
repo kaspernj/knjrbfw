ce5a11cec68fa2cc9747c0ca78b6a1356b03ab08
Kasper Johansen <k@spernj.org>
2012-06-10 15:09:27 +0200

    Added more tests.

--- spec/arrayext_spec.rb
+++ spec/arrayext_spec.rb
@@ -6,5 +6,9 @@ describe "ArrayExt" do
     Knj::ArrayExt.force_no_cols(:arr => arr, :no => 1)
     raise "Expected length of 1 but got: #{arr.length}" if arr.length != 1
     raise "Expected element to be 1 but it wasnt: #{arr[0]}" if arr[0] != 1
+    
+    Knj::ArrayExt.force_no_cols(:arr => arr, :no => 3, :empty => "test")
+    raise "Expected length of 3 but got: #{arr.lengtj}" if arr.length != 3
+    raise "Expected element 2 to be 'test' but it wasnt: #{arr[2]}" if arr[2] != "test"
   end
 end
