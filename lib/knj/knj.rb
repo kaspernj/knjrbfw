771e9ef60ed4af4b26df6b1b7443d56883ea7d73
Kasper Johansen <k@spernj.org>
2012-08-24 10:17:32 +0200

    Added gem-loading-method which can load development versions of a gem to enable development.

--- lib/knj/knj.rb
+++ lib/knj/knj.rb
@@ -44,4 +44,32 @@ module Knj
       return Array_enumerator.new(args[:enum])
     end
   end
+  
+  #Loads a gem by a given name. First tries to load the gem from a custom parent directory to enable loading of development-gems.
+  def self.gem_require(gem_const, gem_name)
+    #Return false if the constant is already loaded.
+    return false if ::Kernel.const_defined?(gem_const)
+    
+    #Try to load gem from custom development-path.
+    found_custom = false
+    
+    paths = [
+      "#{File.dirname(__FILE__)}/../../../#{gem_name}/lib/#{gem_name}.rb"
+    ]
+    paths.each do |path|
+      if File.exists?(path)
+        require path
+        found_custom = true
+        break
+      end
+    end
+    
+    #Custom-path could not be loaded - load gem normally.
+    if !found_custom
+      require gem_name
+    end
+    
+    #Return true to enable detection of that something was loaded.
+    return true
+  end
 end
