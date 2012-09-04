6ecea1e1c1ba2131a68e71c95b0254c9a7cb63c5
Kasper Johansen <k@spernj.org>
2012-09-04 09:56:36 +0200

    Added method to add new models. Added method to resort the treeview according to model rules.

--- lib/knj/gtk2_cb.rb
+++ lib/knj/gtk2_cb.rb
@@ -14,37 +14,25 @@ class Gtk::ComboBox
       :items => []
     }
     
-    ls = Gtk::ListStore.new(String, String)
+    @ls = Gtk::ListStore.new(String, String)
     cr = Gtk::CellRendererText.new
     self.pack_start(cr, false)
     self.add_attribute(cr, "text", 0)
     
     if items.is_a?(Array)
       items.each do |appendob|
-        iter = ls.append
-        
         if appendob.is_a?(String)
+          iter = @ls.append
           iter[0] = appendob
         elsif appendob.respond_to?(:is_knj?)
-          if appendob.respond_to?(:name)
-            iter[0] = appendob.name
-          elsif appendob.respond_to?(:title)
-            iter[0] = appendob.title
-          else
-            raise "Could not figure out of the name of the object."
-          end
-          
-          @knj[:items] << {
-            :iter => iter,
-            :object => appendob
-          }
+          self.append_model(:model => appendob)
         end
       end
     elsif items.is_a?(Hash)
       @knj[:type] = :hash
       
       items.each do |key, val|
-        iter = ls.append
+        iter = @ls.append
         iter[0] = val
         
         @knj[:items] << {
@@ -56,8 +44,29 @@ class Gtk::ComboBox
       raise "Unsupported type: '#{items.class.name}'."
     end
     
-    self.model = ls
+    self.model = @ls
     self.active = 0
+  end
+  
+  #Appens a model to the list-store.
+  def append_model(args)
+    iter = @ls.append
+    appendob = args[:model]
+    
+    if appendob.respond_to?(:name)
+      iter[0] = appendob.name
+    elsif appendob.respond_to?(:title)
+      iter[0] = appendob.title
+    else
+      raise "Could not figure out of the name of the object."
+    end
+    
+    @knj[:items] << {
+      :iter => iter,
+      :object => appendob
+    }
+    
+    return {}
   end
   
   def sel
@@ -106,5 +115,24 @@ class Gtk::ComboBox
     end
     
     raise "Could not find such a row: '#{actob}'."
+  end
+  
+  def resort
+    @ls.set_sort_column_id(0)
+    @ls.set_sort_func(0, &lambda{|iter1, iter2|
+      item_id_1 = iter1[1].to_i
+      item_id_2 = iter2[1].to_i
+      
+      item_name_1 = iter1[0].to_s.downcase
+      item_name_2 = iter2[0].to_s.downcase
+      
+      if item_id_1 == 0
+        return -1
+      elsif item_id_2 == 0
+        return 1
+      else
+        return item_name_1 <=> item_name_2
+      end
+    })
   end
 end
