be4e5b7fdc559cc3f9ee432ecf64a1c47642009d
Kasper Johansen <k@spernj.org>
2012-10-23 12:28:20 +0200

    Moved matching above Internet Explorer because bot masks itself as Internet Explorer.

--- lib/knj/web.rb
+++ lib/knj/web.rb
@@ -601,6 +601,10 @@ class Knj::Web
       browser = "firefox"
       title = "Mozilla Firefox"
       version = match[1]
+    elsif servervar["HTTP_ACCEPT"] == "*/*" and servervar["HTTP_ACCEPT_LANGUAGE"] == "zh-cn,zh-hk,zh-tw,en-us" and servervar["HTTP_USER_AGENT"] == "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" and (servervar["REMOTE_ADDR"][0,10] == "114.80.93." or servervar["HTTP_X_FORWARDED_FOR"][0,10] == "114.80.93.")
+      browser = "bot"
+      title = "bot"
+      version = "(unknown annoying bot from China)"
     elsif match = agent.match(/msie\s*(\d+\.\d+)/)
       browser = "ie"
       title = "Microsoft Internet Explorer"
@@ -693,10 +697,6 @@ class Knj::Web
       browser = "bot"
       title = "Bot"
       version = "Sosospider"
-    elsif servervar["HTTP_ACCEPT"] == "*/*" and servervar["HTTP_ACCEPT_LANGUAGE"] == "zh-cn,zh-hk,zh-tw,en-us" and servervar["HTTP_USER_AGENT"] == "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" and (servervar["REMOTE_ADDR"] == "114.80.93.55" or servervar["HTTP_X_FORWARDED_FOR"] == "114.80.93.55")
-      browser = "bot"
-      title = "bot"
-      version = "(unknown annoying bot)"
     else
       browser = "unknown"
       title = "(unknown browser)"
